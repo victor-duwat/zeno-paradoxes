2ab5f8a8c090203d3f837e0c17e17d8a78840573
helio-aubrun <helio.aubrun@laplateforme.io>
2024-09-02 16:43:16 +0200

    add Achille.py

---
+++ Achille.py
@@ -0,0 +1,19 @@
+position_achille = 0.0
+position_tortue = 10.0
+
+vitesse_achille = 2.0
+vitesse_tortue = 1.0
+
+for i in range (1000):
+    temps = ( position_tortue - position_achille ) / vitesse_achille
+    
+    position_achille = position_tortue
+
+    position_tortue = position_tortue + (temps * vitesse_tortue)
+
+    print (i+1," temps ",temps)
+    print (" achille ",position_achille)
+    print (" tortue ",position_tortue)
+
+    if position_tortue == position_achille:
+        break
